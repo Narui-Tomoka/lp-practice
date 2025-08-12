b16c1b6400d48c84fa9243c5cbbc9c7f7fd895d8
Tomoka Narui <sweetmelty2512@gmail.com>
2025-08-13 08:35:14 +0900

    今回の課題に必要ない部分（後から足したJSで動く部分）を削除。CSSをリセット

---
+++ main.js
@@ -0,0 +1,10 @@
+// "use strict";
+
+// {
+//   const hamburger = document.querySelector(".hamburger");
+//   const container = document.querySelector(".container");
+//   hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("active");
+//     container.classList.toggle("active");
+//   });
+// }
